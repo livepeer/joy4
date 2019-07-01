47ed8b1baad5670ad5cfa6813e60a940c65b2ba9
Ivan Tivonenko <darkdarkdragon@gmail.com>
2019-07-01 19:56:20 +0300

    change all the link to point to Livepeer's repo

--- codec/codec.go
+++ codec/codec.go
@@ -1,8 +1,8 @@
 package codec
 
 import (
-	"github.com/nareix/joy4/av"
-	"github.com/nareix/joy4/codec/fake"
+	"github.com/livepeer/joy4/av"
+	"github.com/livepeer/joy4/codec/fake"
 	"time"
 )
 
--- format/format.go
+++ format/format.go
@@ -1,13 +1,13 @@
 package format
 
 import (
-	"github.com/nareix/joy4/format/mp4"
-	"github.com/nareix/joy4/format/ts"
-	"github.com/nareix/joy4/format/rtmp"
-	"github.com/nareix/joy4/format/rtsp"
-	"github.com/nareix/joy4/format/flv"
-	"github.com/nareix/joy4/format/aac"
-	"github.com/nareix/joy4/av/avutil"
+	"github.com/livepeer/joy4/format/mp4"
+	"github.com/livepeer/joy4/format/ts"
+	"github.com/livepeer/joy4/format/rtmp"
+	"github.com/livepeer/joy4/format/rtsp"
+	"github.com/livepeer/joy4/format/flv"
+	"github.com/livepeer/joy4/format/aac"
+	"github.com/livepeer/joy4/av/avutil"
 )
 
 func RegisterAll() {
